88d028dd4371a2413810f0f7fc784c81ca280355
wesleejacop <142736134+wesleejacop@users.noreply.github.com>
2025-09-27 22:09:50 +0530

    Update and rename readme.m to readme.md

--- week 1/readme.m
+++
@@ -1,6 +0,0 @@
-# Day 1: Introduction to Verilog RTL Design & Synthesis
-
-Welcome to my **Day 1** RTL Workshop report!  
-Today, I embarked on my journey into digital design by learning Verilog, open-source simulation with **Icarus Verilog (iverilog)**, and the basics of logic synthesis using **Yosys**. This report documents my practical labs, key learnings, and hands-on experiences as I built a strong foundation in RTL design.
-
----
